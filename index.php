5225b4ebd08174a67c16e8f6dfa5e5f886d9ecc9
SahanDilusha <sahancodelygroup@gmail.com>
2024-04-13 19:28:56 +0530

    "Added container, row, and column divs to center content; added image and form inputs for email and password; added forgot password button"

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Krist Admin Login</title>
@@ -8,17 +9,38 @@
     <link rel="stylesheet" href="bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-  
-  <div class="">
+</head>
 
-  </div>
+<body class="bg-body-secondary">
 
+    <div class="container-fluid min-vh-100">
 
+        <div class="row d-flex flex-column justify-content-center align-items-center min-vh-100">
+            <div class="col-md-6 p-3 bg-white rounded-3 col-lg-4 d-flex flex-column justify-content-center align-items-center">
+                <img src="resources/image/Logo.png" alt="">
+
+                <div class="w-100 mt-4 mb-4">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mt-4">
+                        <button class="btn btn-dark px-3">Login</button>
+                        <button class="btn bg-transparent border-0 text-primary fw-bold">forgot password?</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 
 </body>
+
 </html>
