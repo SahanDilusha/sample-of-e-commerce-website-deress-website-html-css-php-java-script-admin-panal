674b6f9a6e5598b6df1f9ffa1c772a4e85939f00
SahanDilusha <sahancodelygroup@gmail.com>
2024-04-13 20:10:54 +0530

    "Changed login form input fields from email to username and password"
    This commit message describes the changes made in the code diff by accurately pointing out that the input fields in the login form have been modified from email to username and password. It is

--- index.php
+++ index.php
@@ -21,12 +21,12 @@
 
                 <div class="w-100 mt-4 mb-4">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" required/>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" required/>
                     </div>
                     <div class="d-flex justify-content-between align-items-center mt-4">
                         <button class="btn btn-dark px-3">Login</button>
@@ -37,6 +37,10 @@
         </div>
 
     </div>
+
+    <?php 
+     include "modle-erro.php";
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="script.js"></script>
---
+++ modle-erro.php
@@ -0,0 +1,17 @@
+<!-- Modal -->
+<div class="modal fade" id="error-model" tabindex="-1" aria-labelledby="error-modelLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="error-modelLabel">Message</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <label for="text-erro" id="text-erro"></label>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
