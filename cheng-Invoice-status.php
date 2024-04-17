619006672267f2ae82711dba08e44568d2e1bce8
SahanDilusha <sahancodelygroup@gmail.com>
2024-04-17 12:44:38 +0530

    "Update invoice status only if the invoice exists and the new status is different"
    This commit updates the `cheng-Invoice-status.php` file to include additional checks when updating the status of an invoice. It

--- cheng-Invoice-status.php
+++ cheng-Invoice-status.php
@@ -5,6 +5,19 @@ include "connecton.php";
 if (isset($_POST["st"]) || isset($_POST["id"])) {
 
     if ($_POST["st"] != "" && $_POST["id"] != "") {
-        
+
+        $checkInvoice = Database::search("SELECT * FROM `invoice` WHERE `invoice`.`invoice_id` = '" . $_POST["id"] . "';");
+
+        if ($checkInvoice->num_rows == 1) {
+            $checkInvoice->fetch_assoc();
+
+            if ($_POST["st"] !== $checkInvoice["stetus_stetus_id"]) {
+
+                Database::iud("UPDATE `invoice` SET `invoice`.`stetus_stetus_id` = '" . $_POST["st"] . "' WHERE `invoice`.`invoice_id` = '" . $_POST["id"] . "';");
+                echo ('status changed');
+            }
+        } else {
+            echo ("Error: Invoice not fuond!");
+        }
     }
 }
